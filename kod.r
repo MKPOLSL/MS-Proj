bbaa2d657fe443815f8dea88e9cde6385bba4efc
Mateusz Kulinowski <43930344+Vuziks@users.noreply.github.com>
2020-04-08 11:00:25 +0200

    Add files via upload

--- kod.r
+++ kod.r
@@ -3,13 +3,12 @@
 dane <- read.csv2("dane.csv", sep=";")
 stara_hala <- as.vector(dane[[1]], mode = "double")
 nowa_hala <- as.vector(dane[[2]], mode = "double")
-nowa_hala <- na.omit(nowa_hala)  #nie uwzględniaj NA
+nowa_hala <- na.omit(nowa_hala)  #nie uwzglÄ™dniaj NA
 
-#miary zróżnicowania pozycyjne
-rozstep_stara = max(stara_hala) - min(stara_hala)
-rozstep_nowa = max(nowa_hala) - min(nowa_hala)
+source('funkcje.r')
 
-#miary przeciętne - średnia arytmetyczna (szereg szczegółowy)
+#miary przeciÄ™tne - Ĺ›rednia arytmetyczna (szereg szczegĂłĹ‚owy)
+
 suma_stara <- sum(stara_hala)
 liczba_stara <- length(stara_hala)
 
@@ -59,25 +58,13 @@ odchylenie_cwiartkowe_stara <- (kwantyl_75_stara - kwantyl_25_stara)/2
 wspolczynnik_pozycyjny_nowa <- (odchylenie_cwiartkowe_nowa/mediana_nowa)*100
 wspolczynnik_pozycyjny_stara <- (odchylenie_cwiartkowe_stara/mediana_stara)*100
 
-#miary zróżnicowania względne
 wspolczynnik_zmiennosci_nowa <- (odchylenie_standardowe_nowa/srednia_nowa)*100
 wspolczynnik_zmiennosci_stara <- (odchylenie_standardowe_stara/srednia_stara)*100
 
-histogram_nowa <- hist(nowa_hala)
-histogram_stara <- hist(stara_hala)
+hist(nowa_hala)
+hist(stara_hala)
 
-#miary asymetrii  
-wspolczynnik_asymetrii_stara = (sum((sort(stara_hala) - srednia_stara) ^ 3) / length(stara_hala))/ (odchylenie_standardowe_stara ^ 3 )
-wspolczynnik_asymetrii_nowa = (sum((sort(nowa_hala) - srednia_nowa) ^ 3) / length(nowa_hala))/ (odchylenie_standardowe_nowa ^ 3 )
-
-#miary koncentracji
-kurtoza_stara = (sum((sort(stara_hala) - srednia_stara) ^ 4) / length(sort(stara_hala))) / (odchylenie_standardowe_stara ^ 4)
-kurtoza_nowa = (sum((sort(nowa_hala) - srednia_nowa) ^ 4) / length(sort(nowa_hala))) / (odchylenie_standardowe_nowa ^ 4)
-
-eksces_stara = kurtoza_stara - 3
-eksces_nowa = kurtoza_nowa - 3
-
-#szereg przedziałowy
+#szereg przedziaĹ‚owy
 nowa_przedzial <- cut(nowa_hala, sqrt(liczba_nowa))
 stara_przedzial <- cut(stara_hala, sqrt(liczba_stara))
 
@@ -95,11 +82,5 @@ wariancja_przedzial_nowa = sum(((nowa_przedzial$mids-srednia_przedzial_nowa) ^ 2
 
 nowa_przedzial_srednia <- sum(nowa_przedzial$counts * nowa_przedzial$mids)/sum(nowa_przedzial$counts)
 
-wspolczynnik_asymetrii_nowa_przedzialowy = (sum((histogram_nowa$mids - srednia_nowa) ^ 3 * histogram_nowa$counts) / sum(histogram_nowa$counts)) / (odchylenie_standardowe_nowa ^ 3)
-wspolczynnik_asymetrii_stara_przedzialowy = (sum((histogram_stara$mids - srednia_stara) ^ 3 * histogram_stara$counts) / sum(histogram_stara$counts)) / (odchylenie_standardowe_stara ^ 3)
-
-kurtoza_stara_przedzialowa = (sum((histogram_stara$mids - srednia_stara) ^ 4 * histogram_stara$counts) / sum(histogram_stara$counts)) / (odchylenie_standardowe_stara ^ 4)
-kurtoza_nowa_przedzialowa =  (sum((histogram_nowa$mids - srednia_nowa) ^ 4 * histogram_nowa$counts) / sum(histogram_nowa$counts)) / (odchylenie_standardowe_nowa ^ 4)
-
-eksces_stara_przedzialowy = kurtoza_stara_przedzialowa - 3
-eksces_nowa_przedzialowy = kurtoza_nowa_przedzialowa - 3
+a <- 20
+b <- funkja_testowa(a)
