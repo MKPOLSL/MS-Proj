2c8f9d04b7885c6a1fe9589028820f87fb64cf0a
paweryk807 <bambek7@gmail.com>
2020-05-26 21:41:21 +0200

    PORZONTKI

--- funkcje.r
+++ funkcje.r
@@ -1,7 +1,19 @@
-funkcja_testowa <- function(a){
-  return (a * 2)
+
+moment_centralny <- function(a, n) {
+  ilosc <- length(a)
+  srednia <- sum(a) / ilosc
+  wynik <- 0
+  for(val in a) {
+    wynik <- wynik + (val - srednia) ^ n
+  }
+  wynik <- wynik / ilosc
+  return(wynik)
 }
 
+modalna <- function(a) {
+  wynik <- unique(a)
+  wynik[which.max(tabulate(match(a, wynik)))]
+}
 
 #kod zajebany
 test_zgodnosci <- function(a){
@@ -13,16 +25,16 @@ test_zgodnosci <- function(a){
   dystrybuanta_empiryczna <- dane
   dystrybuanta_hipotetyczna <-dane
   roznica <- dane
-  tablica_rozkladu = 0.264 # Statystyka z tablicy rozkÅ‚adu KoÅ‚mogorowa
+  tablica_rozkladu = 0.264 # Statystyka z tablicy rozk³adu Ko³mogorowa
   
   # Standaryzacja
   for(i in 1:length(a)) {
     standaryzacja[i] <- (dane[i]-sredniaa)/odchStanda
   }
   
-  # Dystrybuanta rozkÅ‚adu hipotetycznego
+  # Dystrybuanta rozk³adu hipotetycznego
   for(i in 1:length(a)) {
-    dystrybuanta_hipotetyczna[i] <- pnorm(standaryzacja[i]) #pnorm zwraca funkcjÄ™ dystrybuanty
+    dystrybuanta_hipotetyczna[i] <- pnorm(standaryzacja[i]) #pnorm zwraca funkcjê dystrybuanty
   }
   
   # Dystrybuanta empiryczna
@@ -30,25 +42,66 @@ test_zgodnosci <- function(a){
     dystrybuanta_empiryczna[i] <- i/length(a)
   }
   
-  # RÃ³Å¼nica dystrybuant
+  # Ró¿nica dystrybuant
   for(i in 1:length(a)) {
     roznica[i] <- abs(dystrybuanta_hipotetyczna[i] - dystrybuanta_empiryczna[i])
   }
   
-  # WartoÅ›Ä‡ statystyki testowej
+  # Wartoœæ statystyki testowej
   wartosc_statystyki_testowej <- max(roznica)
   
   
-  writeLines("H0 - rozkÅ‚ad normalny\n")
-  writeLines("H1 - brak rozkÅ‚adu normalnego\n")
+  writeLines("H0 - rozk³ad normalny\n")
+  writeLines("H1 - brak rozk³adu normalnego\n")
   
   writeLines("dla aycyjnego systemu:\n")
   if(wartosc_statystyki_testowej < tablica_rozkladu || wartosc_statystyki_testowej> 1){
     writeLines("Brak podstaw do odrzucenia hipotezy H0\n")
-    writeLines("Dane majÄ… rozkÅ‚ad normalny\n")
+    writeLines("Dane maj¹ rozk³ad normalny\n")
   }else{
-    writeLines("IstniejÄ… podstawy do odrzucenia hipotezy H0\n")
-    writeLines("Dane nie majÄ… rozkÅ‚adu normalnego\n")
+    writeLines("Istniej¹ podstawy do odrzucenia hipotezy H0\n")
+    writeLines("Dane nie maj¹ rozk³adu normalnego\n")
   }
+}
+
+wspolczynnik_TStudenta <- function(ufn, n) {
+  return(qt((1 - ufn) / 2, n - 1, lower.tail = FALSE, log.p = FALSE))
+}
+
+# Przedzia³ ufnoœci dla œredniej
+dolna_granica_sred <- function(srednia, wspT, odch, licz) {
+  dolna_granica = srednia - wspT * (odch / sqrt(licz - 1))
+  return(dolna_granica)
+}
+
+gorna_granica_sred <- function(srednia, wspT, odch, licz) {
+  gorna_granica = srednia + wspT * (odch / sqrt(licz - 1))
+  return(gorna_granica)
+}
+
+wspChiKwadrat<-function(ufn,n){
+  return (qchisq(ufn,n-1))
+}
+#Granice przedzia³u ufnoœci dla wariancji wartoœci pracy nowej hali
+granica_wariancja<-function(n,war,wspChi){
+  return(n*war/wspChi)
+}
+#Wzglêdna precyzja oszacowania
+wzgledna_precyzja_wariancja <- function(dolnaGranica,gornaGranica,war){
+  return(0.5*((gornaGranica-dolnaGranica)/war))
+}
+
+statystyka <- function(x, y) {
+  # Funkcja zwraca wartoœæ statystyki testowej dla porównania
+  # dwóch œrednich z prób podanych jako argumenty
+  x.srednia <- mean(x)
+  x.wariancja <- var(x)
+  y.srednia <- mean(y)
+  y.wariancja <- var(y)
+  
+  U = (x.srednia - y.srednia) /
+    sqrt( (x.wariancja / length(x)) + (y.wariancja / length(y)) )
+  
+  return(U)
 }
 #koniec kodu zajebanego
--- kod.r
+++ kod.r
@@ -3,15 +3,11 @@
 dane <- read.csv2("dane.csv", sep=";")
 stara_hala <- as.vector(dane[[1]], mode = "double")
 nowa_hala <- as.vector(dane[[2]], mode = "double")
-nowa_hala <- na.omit(nowa_hala)  #nie uwzglÄ™dniaj NA
+nowa_hala <- na.omit(nowa_hala)  #nie uwzglêdniaj NA
 
 source('funkcje.r')
 
-#miary zrÃ³Å¼nicowania pozycyjne
-rozstep_stara = max(stara_hala) - min(stara_hala)
-rozstep_nowa = max(nowa_hala) - min(nowa_hala)
-
-#miary przeciÄ™tne - Å›rednia arytmetyczna (szereg szczegÃ³Å‚owy)
+#miary po³o¿enia (szereg szczegó³ówy)
 suma_stara <- sum(stara_hala)
 liczba_stara <- length(stara_hala)
 
@@ -21,44 +17,11 @@ liczba_nowa <- length(nowa_hala)
 srednia_stara <- suma_stara / liczba_stara
 srednia_nowa <- suma_nowa / liczba_nowa
 
-#miary zroznicowania klasyczne
-wariancja_nowa <- 0
-wariancja_stara <- 0
-odch_przecietne_nowa <- 0
-odch_przecietne_stara <- 0
-for(val in nowa_hala)
-{
-  wariancja_nowa <- wariancja_nowa + (val-srednia_nowa)^2/liczba_nowa
-}
-for(val in nowa_hala)
-{
-  odch_przecietne_nowa <- odch_przecietne_nowa + abs(val-srednia_nowa)/liczba_nowa
-}
-for(val in stara_hala)
-{
-  wariancja_stara <- wariancja_stara + (val-srednia_stara)^2/liczba_stara
-}
-for(val in stara_hala)
-{
-  odch_przecietne_stara <- odch_przecietne_stara + abs(val-srednia_stara)/liczba_stara
-}
-
-odchylenie_standardowe_nowa <- sqrt(wariancja_nowa)
-odchylenie_standardowe_stara <- sqrt(wariancja_stara)
-
 mediana_nowa <- median(nowa_hala)
 mediana_stara <- median(stara_hala)
 
-odch_przecietne_od_mediany_nowa <- 0
-odch_przecietne_od_mediany_stara <- 0
-for(val in nowa_hala)
-{
-  odch_przecietne_od_mediany_nowa <- odch_przecietne_od_mediany_nowa + abs(val-mediana_nowa)/liczba_nowa
-}
-for(val in stara_hala)
-{
-  odch_przecietne_od_mediany_stara <- odch_przecietne_od_mediany_stara + abs(val-mediana_stara)/liczba_stara
-}
+moda_stara <- modalna(stara_hala)
+moda_nowa <- modalna(nowa_hala)
 
 kwantyl_25_nowa <- quantile(nowa_hala, probs=(0.25))
 kwantyl_75_nowa <- quantile(nowa_hala, probs=(0.75))
@@ -66,72 +29,102 @@ kwantyl_75_nowa <- quantile(nowa_hala, probs=(0.75))
 kwantyl_25_stara <- quantile(stara_hala, probs=(0.25))
 kwantyl_75_stara <- quantile(stara_hala, probs=(0.75))
 
+#miary rozproszenia
+wariancja_nowa <- moment_centralny(nowa_hala, 2)
+wariancja_stara <- moment_centralny(stara_hala, 2)
+
+odchylenie_standardowe_nowa <- sqrt(wariancja_nowa)
+odchylenie_standardowe_stara <- sqrt(wariancja_stara)
+
+odch_przecietne_nowa <- 0
+odch_przecietne_stara <- 0
+
+for(val in nowa_hala){
+  odch_przecietne_nowa <- odch_przecietne_nowa + abs(val-srednia_nowa)/liczba_nowa
+}
+for(val in stara_hala){
+  odch_przecietne_stara <- odch_przecietne_stara + abs(val-srednia_stara)/liczba_stara
+}
+
+odch_przecietne_od_mediany_nowa <- 0
+odch_przecietne_od_mediany_stara <- 0
+
+for(val in nowa_hala){
+  odch_przecietne_od_mediany_nowa <- odch_przecietne_od_mediany_nowa + abs(val-mediana_nowa)/liczba_nowa
+}
+for(val in stara_hala){
+  odch_przecietne_od_mediany_stara <- odch_przecietne_od_mediany_stara + abs(val-mediana_stara)/liczba_stara
+}
+
 odchylenie_cwiartkowe_nowa <- (kwantyl_75_nowa - kwantyl_25_nowa)/2
 odchylenie_cwiartkowe_stara <- (kwantyl_75_stara - kwantyl_25_stara)/2
 
 wspolczynnik_pozycyjny_nowa <- (odchylenie_cwiartkowe_nowa/mediana_nowa)*100
 wspolczynnik_pozycyjny_stara <- (odchylenie_cwiartkowe_stara/mediana_stara)*100
 
-#miary zrÃ³Å¼nicowania wzglÄ™dne
 wspolczynnik_zmiennosci_nowa <- (odchylenie_standardowe_nowa/srednia_nowa)*100
 wspolczynnik_zmiennosci_stara <- (odchylenie_standardowe_stara/srednia_stara)*100
 
-histogram_nowa <- hist(nowa_hala)
-histogram_stara <- hist(stara_hala)
+#miary asymetrii i koncentracji 
 
-#miary asymetrii  
-wspolczynnik_asymetrii_stara = (sum((sort(stara_hala) - srednia_stara) ^ 3) / length(stara_hala))/ (odchylenie_standardowe_stara ^ 3 )
-wspolczynnik_asymetrii_nowa = (sum((sort(nowa_hala) - srednia_nowa) ^ 3) / length(nowa_hala))/ (odchylenie_standardowe_nowa ^ 3 )
+wspolczynnik_asymetrii_stara <- moment_centralny(stara_hala, 3) / odchylenie_standardowe_stara ^ 3 
+wspolczynnik_asymetrii_nowa <- moment_centralny(nowa_hala, 3) / odchylenie_standardowe_nowa ^ 3 
 
-#miary koncentracji
-kurtoza_stara = (sum((sort(stara_hala) - srednia_stara) ^ 4) / length(sort(stara_hala))) / (odchylenie_standardowe_stara ^ 4)
-kurtoza_nowa = (sum((sort(nowa_hala) - srednia_nowa) ^ 4) / length(sort(nowa_hala))) / (odchylenie_standardowe_nowa ^ 4)
+kurtoza_stara <- moment_centralny(stara_hala, 4) / odchylenie_standardowe_stara ^ 4
+kurtoza_nowa <- moment_centralny(nowa_hala, 4) / odchylenie_standardowe_nowa ^ 4
 
 eksces_stara = kurtoza_stara - 3
 eksces_nowa = kurtoza_nowa - 3
 
-#szereg przedziaÅ‚owy
-nowa_przedzial <- cut(nowa_hala, sqrt(liczba_nowa))
-stara_przedzial <- cut(stara_hala, sqrt(liczba_stara))
 
-nowa_przedzial <- hist(nowa_hala)
-stara_przedzial <- hist(stara_hala)
+#szereg przedzia³owy
 
-suma_stara <- sum(stara_przedzial$counts * stara_przedzial$mids)
-suma_nowa <- sum(nowa_przedzial$counts * nowa_przedzial$mids)
+# Szerokoœæ przedzia³u klasowego
+szerokosc_stara_hala <- (max(stara_hala) - min(stara_hala)) / ceiling((sqrt(liczba_stara)))
+szerokosc_nowa_hala <- (max(nowa_hala) - min(nowa_hala)) /  ceiling((sqrt(liczba_nowa)))
 
-srednia_przedzial_stara <- suma_stara / sum(stara_przedzial$counts)
-srednia_przedzial_nowa <- suma_nowa / sum(nowa_przedzial$counts)
+# Punkty przeciêcia
+punkty_przeciecia_stara = seq(min(stara_hala), max(stara_hala), szerokosc_stara_hala)
+punkty_przeciecia_nowa = seq(min(nowa_hala), max(nowa_hala), szerokosc_nowa_hala)
 
-wariancja_przedzial_stara = sum(((stara_przedzial$mids-srednia_przedzial_stara) ^ 2) * stara_przedzial$counts) / sum(stara_przedzial$counts)
-wariancja_przedzial_nowa = sum(((nowa_przedzial$mids-srednia_przedzial_nowa) ^ 2) * nowa_przedzial$counts) / sum(nowa_przedzial$counts)
+nowa_histogram <- hist(nowa_hala, breaks = punkty_przeciecia_nowa)
+stara_histogram <- hist(stara_hala, breaks = punkty_przeciecia_stara)
 
-nowa_przedzial_srednia <- sum(nowa_przedzial$counts * nowa_przedzial$mids)/sum(nowa_przedzial$counts)
+suma_stara <- sum(stara_histogram$counts * stara_histogram$mids)
+suma_nowa <- sum(nowa_histogram$counts * nowa_histogram$mids)
 
-wspolczynnik_asymetrii_nowa_przedzialowy = (sum((histogram_nowa$mids - srednia_nowa) ^ 3 * histogram_nowa$counts) / sum(histogram_nowa$counts)) / (odchylenie_standardowe_nowa ^ 3)
-wspolczynnik_asymetrii_stara_przedzialowy = (sum((histogram_stara$mids - srednia_stara) ^ 3 * histogram_stara$counts) / sum(histogram_stara$counts)) / (odchylenie_standardowe_stara ^ 3)
+srednia_przedzial_stara <- suma_stara / sum(stara_histogram$counts)
+srednia_przedzial_nowa <- suma_nowa / sum(nowa_histogram$counts)
 
-kurtoza_stara_przedzialowa = (sum((histogram_stara$mids - srednia_stara) ^ 4 * histogram_stara$counts) / sum(histogram_stara$counts)) / (odchylenie_standardowe_stara ^ 4)
-kurtoza_nowa_przedzialowa =  (sum((histogram_nowa$mids - srednia_nowa) ^ 4 * histogram_nowa$counts) / sum(histogram_nowa$counts)) / (odchylenie_standardowe_nowa ^ 4)
+wariancja_przedzial_stara = sum(((stara_histogram$mids-srednia_przedzial_stara) ^ 2) * stara_histogram$counts) / sum(stara_histogram$counts)
+wariancja_przedzial_nowa = sum(((nowa_histogram$mids-srednia_przedzial_nowa) ^ 2) * nowa_histogram$counts) / sum(nowa_histogram$counts)
+
+wspolczynnik_asymetrii_nowa_przedzialowy = (sum((nowa_histogram$mids - srednia_nowa) ^ 3 * nowa_histogram$counts) / sum(nowa_histogram$counts)) / (odchylenie_standardowe_nowa ^ 3)
+wspolczynnik_asymetrii_stara_przedzialowy = (sum((stara_histogram$mids - srednia_stara) ^ 3 * stara_histogram$counts) / sum(stara_histogram$counts)) / (odchylenie_standardowe_stara ^ 3)
+
+kurtoza_stara_przedzialowa = (sum((stara_histogram$mids - srednia_przedzial_stara) ^ 4 * stara_histogram$counts) / sum(stara_histogram$counts)) / (odchylenie_standardowe_stara ^ 4)
+kurtoza_nowa_przedzialowa =  (sum((nowa_histogram$mids - srednia_przedzial_nowa) ^ 4 * nowa_histogram$counts) / sum(nowa_histogram$counts)) / (odchylenie_standardowe_nowa ^ 4)
 
 eksces_stara_przedzialowy = kurtoza_stara_przedzialowa - 3
 eksces_nowa_przedzialowy = kurtoza_nowa_przedzialowa - 3
 
-a = 10
-b = funkcja_testowa(a)
 
-#2 prosze uprzejmie sprawdzic czy zmiennatablica rozkladu jest dobrze policzona 
-tablica_rozkladu_stara<-((0.83+length(stara_hala))/(sqrt(length(stara_hala))))-0.01
-tablica_rozkladu_nowa<-((0.83+length(nowa_hala))/(sqrt(length(nowa_hala))))-0.01
-srednia_stara=sum(stara_hala)/length(stara_hala)
-srednia_nowa=sum(nowa_hala)/length(nowa_hala)
+#ZADANIE 2
+
+#prosze uprzejmie sprawdzic czy zmiennatablica rozkladu jest dobrze policzona 
+
+tablica_rozkladu_stara <- ((0.83+length(stara_hala))/(sqrt(length(stara_hala))))-0.01
+tablica_rozkladu_nowa <- ((0.83+length(nowa_hala))/(sqrt(length(nowa_hala))))-0.01
+srednia_stara <- sum(stara_hala)/length(stara_hala)
+srednia_nowa <- sum(nowa_hala)/length(nowa_hala)
 
 #sortowansko starej i nowej 
-dane_stara <-sort(stara_hala)
-dane_nowa<-sort(nowa_hala)
-#standaryzacja starej
-standaryzacja_stara<-dane_stara
-for(i in stara_hala)
+
+dane_stara <- sort(stara_hala)
+dane_nowa <- sort(nowa_hala)
+
+standaryzacja_stara <- dane_stara
+for(i in dane_stara)
 {
   standaryzacja_stara[i]<-(dane_stara[i]-srednia_stara)/odchylenie_standardowe_stara
 }
@@ -144,13 +137,14 @@ for(i in nowa_hala)
 #dystrybuanta rozkladu hipotetycznego starej
 dystrybuanta_hipotetyczna_stara<-dane_stara
 for(i in stara_hala) {
-  dystrybuanta_hipotetyczna_stara[i] <- pnorm(standaryzacja_stara[i]) #pnorm zwraca funkcjÄ™ dystrybuanty
+  dystrybuanta_hipotetyczna_stara[i] <- pnorm(standaryzacja_stara[i]) #pnorm zwraca funkcjê dystrybuanty
 }
 
 dystrybuanta_hipotetyczna_nowa<-dane_nowa
 for(i in nowa_hala) {
-  dystrybuanta_hipotetyczna_nowa[i] <- pnorm(standaryzacja_nowa[i]) #pnorm zwraca funkcjÄ™ dystrybuanty
+  dystrybuanta_hipotetyczna_nowa[i] <- pnorm(standaryzacja_nowa[i]) #pnorm zwraca funkcjê dystrybuanty
 }
+print(dystrybuanta_hipotetyczna_nowa)
 
 #dystrybuanta empiryczna starej
 dystrybuanta_empiryczna_stara<-dane_stara
@@ -204,60 +198,34 @@ if(wartosc_statystyki_testowej_nowa < tablica_rozkladu_nowa || wartosc_statystyk
 #x <- test_zgodnosci(stara_hala)
 #y <- test_zgodnosci(nowa_hala)
 
-# zadanie 3
-# WspÃ³Å‚czynnik ufnoÅ›ci 1 - alfa = 0.95
-# stÄ…d alfa = 0.05
-wspolczynnik_TStudenta <- function(ufn, n) {
-  return(qt((1 - ufn) / 2, n - 1, lower.tail = FALSE, log.p = FALSE))
-}
+# ZADANIE 3
+# Wspó³czynnik ufnoœci 1 - alfa = 0.95
+# st¹d alfa = 0.05
 
-# PrzedziaÅ‚ ufnoÅ›ci dla Å›redniej
-dolna_granica_sred <- function(srednia, wspT, odch, licz) {
-  dolna_granica = srednia - wspT * (odch / sqrt(licz - 1))
-  return(dolna_granica)
-}
-
-gorna_granica_sred <- function(srednia, wspT, odch, licz) {
-  gorna_granica = srednia + wspT * (odch / sqrt(licz - 1))
-  return(gorna_granica)
-}
-
-# WzglÄ™dna precyzja oszacowania
+# Wzglêdna precyzja oszacowania
 precyzjaSred <- function(gg, dg, sr){
   precyzjaOszacowaniaTrad = 0.5 * (gg - dg) / sr
   return(precyzjaOszacowaniaTrad)
 }
 
-# PrzedziaÅ‚ ufnoÅ›ci dla starej hali
+# Przedzia³ ufnoœci dla starej hali
 granica_dolna_stara <- dolna_granica_sred(srednia_stara, wspolczynnik_TStudenta(0.95, liczba_stara), odchylenie_standardowe_stara, liczba_stara)
 granica_gorna_stara <- gorna_granica_sred(srednia_stara, wspolczynnik_TStudenta(0.95, liczba_stara), odchylenie_standardowe_stara, liczba_stara)
 # przedzial <46, 52>
-# srednia = 49 wiÄ™c zawiera siÄ™ w przedziale ufnoÅ›ci
+# srednia = 49 wiêc zawiera siê w przedziale ufnoœci
 
 # Precyzja oszacowania dla starej hali
 precyzja_stara <- precyzjaSred(granica_gorna_stara, granica_dolna_stara, srednia_stara)
 
 # zadanie 4
-# WspÃ³Å‚czynnik ufnoÅ›ci 1 - alfa = 0.95
+# Wspó³czynnik ufnoœci 1 - alfa = 0.95
 
-wspChiKwadrat<-function(ufn,n){
-  return (qchisq(ufn,n-1))
-}
-#Granice przedziaÅ‚u ufnoÅ›ci dla wariancji wartoÅ›ci pracy nowej hali
-granica_wariancja<-function(n,war,wspChi){
-  return(n*war/wspChi)
-}
-#WzglÄ™dna precyzja oszacowania
-wzgledna_precyzja_wariancja <- function(dolnaGranica,gornaGranica,war){
-  return(0.5*((gornaGranica-dolnaGranica)/war))
-}
-
-#WspÃ³Å‚czynniki chiKwadrat dla: chi(0.975,42), chi(0.025,42)
+#Wspó³czynniki chiKwadrat dla: chi(0.975,42), chi(0.025,42)
 wspAlfa<-0.05
 ChiKwadrat1<-wspChiKwadrat(wspAlfa/2,liczba_nowa) 
 ChiKwadrat2<-wspChiKwadrat(1-(wspAlfa/2),liczba_nowa)
 
-#PrzedziaÅ‚ ufnoÅ›ci dla nowej hali
+#Przedzia³ ufnoœci dla nowej hali
 gornaGranicaNowa <- granica_wariancja(liczba_nowa,wariancja_nowa,ChiKwadrat1)
 dolnaGranicaNowa <- granica_wariancja(liczba_nowa,wariancja_nowa,ChiKwadrat2)
 
@@ -271,22 +239,8 @@ precyzja_nowa <- wzgledna_precyzja_wariancja(dolnaGranicaNowa,gornaGranicaNowa,w
 
 #zadanie 5
 
-statystyka <- function(x, y) {
-  # Funkcja zwraca wartoÅ›Ä‡ statystyki testowej dla porÃ³wnania
-  # dwÃ³ch Å›rednich z prÃ³b podanych jako argumenty
-  x.srednia <- mean(x)
-  x.wariancja <- var(x)
-  y.srednia <- mean(y)
-  y.wariancja <- var(y)
-  
-  U = (x.srednia - y.srednia) /
-    sqrt( (x.wariancja / length(x)) + (y.wariancja / length(y)) )
-  
-  return(U)
-}
-
-# Hipoteza zerowa: wartoÅ›Ä‡ wydajnoÅ›ci pracy przy produkcji w starej hali sÄ… wiÄ™ksze
-# Hipoteza alternatywna: wartoÅ›Ä‡ wydajnoÅ›ci pracy przy produkcji w starej hali nie sÄ… wiÄ™ksze
+# Hipoteza zerowa: wartoœæ wydajnoœci pracy przy produkcji w starej hali s¹ wiêksze
+# Hipoteza alternatywna: wartoœæ wydajnoœci pracy przy produkcji w starej hali nie s¹ wiêksze
 
 wartosc.statystyki <- statystyka(nowa_hala, stara_hala)
 kwantyl95 <- qnorm(0.95)
